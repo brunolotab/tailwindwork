3c1a45acf136a31ea35a006407c0351485eab7a0
brunolotab <lotadiamond87@gmail.com>
2024-06-18 08:23:51 +0100

    third commit

--- tailwindcss/src/Directors.jsx
+++ tailwindcss/src/Directors.jsx
@@ -7,36 +7,36 @@ import Titilayo from './assets/Dimage1_files/titilayo-8b5544c6.png'
 
 function Directors() {
     return (
-        <div className='flex  flex-col justify-center align-middle w-[100%] p-7'>
+        <div className=' w-[100%] p-7'>
              <div>
-                    <h3 style={{ textAlign: 'center', color: '#24b0ff', fontWeight: 'bolder' }}>Meet Our</h3>
-                    <h1 style={{ textAlign: 'center', fontWeight: 900 }}>Board Of Directors</h1><br />
+                    <h3 style={{ textAlign: 'center', fontSize:'25px',color: '#24b0ff', fontWeight: 'bolder' }}>Meet Our</h3>
+                    <h1 style={{ textAlign: 'center', fontSize:'20px', fontWeight: 900 }}>Board Of Directors</h1><br />
                     
             </div>
-            <div className='p-8 grid grid-cols-3 grid-rows-[350px]'>
-                <div className=' col-start-2 col-end-4'>
-                    <div className='w-[250px] h-[300px]'><img src={Agada} alt='' className='[w-[100%] h-[100%] object-cover'/></div>
-                    <div className='directors-name'><div>Aganda Apoch <span></span></div>
+            <div className='p-8 grid lg:grid-cols-3 justify-items-center gap-8 border border-lime-100 w-[100%] md:grid-cols-2'>
+                <div className=' lg:col-start-2 lg:col-end-3 h-[300px] mb-4'>
+                    <div className='lg:w-[240px] md:w-[240px] vsm:w-[240px]  '><img src={Agada} alt='' className='[w-[100%] object-cover'/></div>
+                    <div className='relative top-[-105px] bg-[#aaa] m-2 leading-9 text-white p-2' ><div className='font-bold'>Aganda Apoch <span></span></div>
                         <p>Chairman</p></div>
                 </div>
-                <div className='Md'>
-                    <div className='w-[250px] h-[300px]'><img src={Albert} alt='' className='[w-[100%] h-[100%] object-cover'/></div>
-                    <div className='directors-name' ><div>Afolabi Albert <span style={{ fontSize: '25', color: 'white', fontWeight: 'bold', float: 'right' }}></span></div>
+                <div className='lg:col-start-1 lg:col-end-2 h-[300px] mb-4' >
+                    <div className='w-[240px]'><img src={Albert} alt='' className='[w-[100%]  object-cover'/></div>
+                    <div className='relative top-[-105px] bg-[#aaa] m-2 leading-9 text-white p-2' ><div>Afolabi Albert <span style={{ fontSize: '25', color: 'white', fontWeight: 'bold', float: 'right' }}></span></div>
                         <p>MD/CEO</p></div>
                 </div>
-                <div>
-                    <div className='w-[250px] h-[300px]'><img src={Titilayo} alt='' className='[w-[100%] h-[100%] object-cover'/></div>
-                    <div className='directors-name'><div>Olubiyi Tititayo<span style={{ fontSize: '25', color: 'white', fontWeight: 'bold', float: 'right' }}></span></div>
+                <div className='h-[300px] mb-4'>
+                    <div className='w-[240px]'><img src={Titilayo} alt='' className='[w-[100%]  object-cover'/></div>
+                    <div className='relative top-[-105px] bg-[#aaa] m-2 leading-9 text-white p-2'><div>Olubiyi Tititayo<span style={{ fontSize: '25', color: 'white', fontWeight: 'bold', float: 'right' }}></span></div>
                         <p>Director</p></div>
                 </div>
-                <div >
-                    <div className='w-[250px] h-[300px]'><img src={Ejembi} alt='' className='[w-[100%] h-[100%] object-cover'/></div>
-                    <div className='directors-name' ><div>Ejembi Emmanuel <span style={{ fontSize: '25', color: 'white', fontWeight: 'bold', float: 'right' }}></span></div>
+                <div className='h-[300px] mb-4'>
+                    <div className='lg:w-[240px] h-[300px]'><img src={Ejembi} alt='' className='[w-[100%] h-[100%] object-cover'/></div>
+                    <div className='relative top-[-105px] bg-[#aaa] m-2 leading-9 text-white p-2'><div>Ejembi Emmanuel <span style={{ fontSize: '25', color: 'white', fontWeight: 'bold', float: 'right' }}></span></div>
                         <p>Director</p></div>
                 </div>
-                <div className='col-start-2 col-end-4'>
-                    <div className='w-[250px] h-[300px]'><img src={Dosumu} alt='' className='[w-[100%] h-[100%] object-cover'/></div>
-                    <div className='directors-name' ><div>Dosumu Sunday <span style={{ fontSize: '25', color: 'white', fontWeight: 'bold', float: 'right' }}></span></div>
+                <div className='lg:col-start-2 lg:col-end-3 md:col-start-1 md:col-span-1 h-[300px]'>
+                    <div className='w-[240px]'><img src={Dosumu} alt='' className='[w-[100%] h-[100%] object-cover'/></div>
+                    <div className='relative top-[-105px] bg-[#aaa] m-2 leading-9 text-white p-2' ><div>Dosumu Sunday <span style={{ fontSize: '25', color: 'white', fontWeight: 'bold', float: 'right' }}></span></div>
                         <p>Director</p></div>
                 </div>
             </div>
--- tailwindcss/src/Profile.jsx
+++ tailwindcss/src/Profile.jsx
@@ -7,12 +7,12 @@ function Profile() {
         <div>
             <div className=' vsm:block md:flex justify-start items-start gap-8 w-[100%] p-8'>
                 <div className='w-[45%] ' data-aos="fade-right" style={{ marginTop: '50px' }}>
-                    <h1 className='font-bold vsm:text-[25px] vsm:w-[200%] md:w-[100%] md:text-[25px] lg:text-3xl xl:text-4xl' style={{color: '#00A9ff'}}>
+                    <h1 className='font-bold vsm:text-[18px] sm:text-[19px] vsm:w-[200%] md:w-[100%] md:text-[25px] lg:text-3xl xl:text-4xl' style={{color: '#00A9ff'}}>
                         Empowering the Future 
                         With Inovative Software.
                     </h1>
                     
-                    <div className='leading-10 vsm:text-[23px] vsm:w-[220%] vsm:text-start vsm:pb-8 md:w-[100%] md:text-[18px] lg:text-[20px] xl:text-[22px] text-gray'  data-aos="fade-right"  data-aos-delay="400">
+                    <div className='md:leading-10 sm:leading-8 vsm: leading-7 vsm:text-[15px] sm: text-[17px] vsm:w-[220%] vsm:text-start vsm:pb-8 md:w-[100%] md:text-[19px] lg:text-[21px] xl:text-[22px] text-gray'  data-aos="fade-right"  data-aos-delay="400">
                         At TM30 Global Limited, we are more than just a software development firm; we are pioneers, trailblazers, and visionaries. Our team of talented engineers, designers, and strategists are dedicated to crafting bespoke software solutions that align with our clients' unique objectives, empowering them to achieve their goals with utmost efficiency and innovation.
                         <div style={{ marginTop: '8px' }}>
                             <button style={{ background: '#00A9ff', border: 'none', fontSize: '16px', padding: '12px 33px', color: '#fff', borderRadius: '5px' }}>
